f78027fb87425195d5fb04cbfada019d7b416697
Javier <jibarra@proyectomapache.cl>
2021-09-14 14:52:31 -0300

    Initialización del Proyecto

---
+++ storage/logs/log-2021-09-14.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-14 05:25:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:25:09 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:25:12 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:25:12 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:25:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:25:14 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:25:42 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:25:42 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:44 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:44 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:45 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:45 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:45 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:45 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2021-09-14 05:27:46 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\laragon\www\padel\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-09-14 05:27:46 --> Severity: error --> Exception: Unable to connect to the database. C:\laragon\www\padel\vendor\codeigniter\framework\system\database\DB_driver.php 433
